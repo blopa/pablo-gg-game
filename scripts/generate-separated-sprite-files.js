0a44e4eee325c248e5f43ff65c2f0ddbd1e84de8
Pablo Pirata <pablo@studocu.com>
2023-02-18 16:53:25 +0100

    add original dir to output

--- scripts/generate-separated-sprite-files.js
+++ scripts/generate-separated-sprite-files.js
@@ -1,12 +1,8 @@
+const path = require('path');
 const sharp = require('sharp');
-const { existsSync, mkdirSync } = require('fs');
 
 async function generateSeparatedSpriteFiles(inputFilePath, spriteWidth, spriteHeight) {
-    const outputDirectory = 'output';
-
-    if (!existsSync(outputDirectory)) {
-        mkdirSync(outputDirectory);
-    }
+    const outputDirectory = path.dirname(inputFilePath);
 
     const metadata = await sharp(inputFilePath).metadata();
     const cols = Math.floor(metadata.width / spriteWidth);
@@ -16,7 +12,7 @@ async function generateSeparatedSpriteFiles(inputFilePath, spriteWidth, spriteHe
         for (let col = 0; col < cols; col++) {
             const left = col * spriteWidth;
             const top = row * spriteHeight;
-            const outputPath = `${outputDirectory}/${row}_${col}.png`;
+            const outputPath = path.join(outputDirectory, `${row}_${col}.png`);
 
             // eslint-disable-next-line no-await-in-loop
             await sharp(inputFilePath)
